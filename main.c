a7a7c34d3050aaaad65d9243e2fcf5f276536b9f
blancoricardo1 <ricardo.blanco0228@gmail.com>
2025-11-23 18:28:01 -0400

    BLE disconnection issues resolved. Removed STATUS_REG read from loop and delay in doSPI (unnecessary apparently). IMU ODR dropped to 26 Hz

--- main.c
+++ main.c
@@ -459,7 +459,7 @@ int main(void)
   MXC_Delay(MXC_DELAY_MSEC(20)); // <- give it time after reset
   i2c_write(CTRL3_C, 0x44);      // BDU=1, IF_INC=1
   i2c_write(CTRL2_G, 0x00);      // gyro off
-  i2c_write(CTRL1_XL, 0x30);     // accel 104Hz
+  i2c_write(CTRL1_XL, 0x20);     // accel 104Hz
   i2c_write(DRDY_PULSE_CFG_G, 0x00);
   i2c_write(INT1_CTRL, 0x01); // XL_DRDY -> INT1
 
@@ -532,55 +532,54 @@ int main(void)
       interrupt = 0;
     }
     /* ---------- IMU SERVICE ---------- */
-    // if (imu_drdy_flag && recordingIMU)
-    // {
-    //   imu_drdy_flag = false;
-    //   uint8_t raw[6];
-    //   int rc = i2c_read(OUTX_L_XL, raw, 6);
-    //   if (rc != 0)
-    //   {
-    //     // printf("[I2C ERR] rc=%d\n", rc);
-    //     imu_err_streak++;
-    //     if (imu_err_streak >= 3)
-    //     {
-    //       // printf("Reinitializing I2C...\n");
-    //       MXC_I2C_Shutdown(I2C_MASTER);
-    //       MXC_I2C_Init(I2C_MASTER, 1, 0);
-    //       MXC_I2C_SetFrequency(I2C_MASTER, I2C_FREQ);
-    //       imu_err_streak = 0;
-    //     }
-    //   }
-    //   else
-    //   {
-    //     imu_err_streak = 0;
-    //     (void)i2c_read(STATUS_REG, &st, 1);
+    if (imu_drdy_flag && recordingIMU)
+    {
+      imu_drdy_flag = false;
+      uint8_t raw[6];
+      int rc = i2c_read(OUTX_L_XL, raw, 6);
+      if (rc != 0)
+      {
+        // printf("[I2C ERR] rc=%d\n", rc);
+        imu_err_streak++;
+        if (imu_err_streak >= 3)
+        {
+          // printf("Reinitializing I2C...\n");
+          MXC_I2C_Shutdown(I2C_MASTER);
+          MXC_I2C_Init(I2C_MASTER, 1, 0);
+          MXC_I2C_SetFrequency(I2C_MASTER, I2C_FREQ);
+          imu_err_streak = 0;
+        }
+      }
+      else
+      {
+        imu_err_streak = 0;
 
-    //     int16_t ax = (int16_t)((raw[1] << 8) | raw[0]);
-    //     int16_t ay = (int16_t)((raw[3] << 8) | raw[2]);
-    //     int16_t az = (int16_t)((raw[5] << 8) | raw[4]);
-    //     // printf("g: X=%.3f Y=%.3f Z=%.3f\n",
-    //     //        (double)ax*LSB_G, (double)ay*LSB_G, (double)az*LSB_G);
-    //     char log_entry[128];
+        int16_t ax = (int16_t)((raw[1] << 8) | raw[0]);
+        int16_t ay = (int16_t)((raw[3] << 8) | raw[2]);
+        int16_t az = (int16_t)((raw[5] << 8) | raw[4]);
+        // printf("g: X=%.3f Y=%.3f Z=%.3f\n",
+        //        (double)ax*LSB_G, (double)ay*LSB_G, (double)az*LSB_G);
+        char log_entry[128];
 
-    //     // Format the log entry with timestamp, Q, I, and F_BIOZ
-    //     int log_len = snprintf(log_entry, sizeof(log_entry), "%.2f,%.2f,%.2f\n", (double)ax * LSB_G, (double)ay * LSB_G, (double)az * LSB_G);
+        // Format the log entry with timestamp, Q, I, and F_BIOZ
+        int log_len = snprintf(log_entry, sizeof(log_entry), "%.2f,%.2f,%.2f\n", (double)ax * LSB_G, (double)ay * LSB_G, (double)az * LSB_G);
 
-    //     datsSendData(AppConnIsOpen(), log_entry, log_len);
-    //     UINT written;
-    //     int err = 0;
-    //     if ((err = f_write(&imuFile, log_entry, log_len, &written)) != FR_OK || written != log_len)
-    //     {
-    //       printf("Write failed: %s\n", FF_ERRORS[err]);
-    //       return err;
-    //     }
-    //   }
+        datsSendData(AppConnIsOpen(), log_entry, log_len);
+        UINT written;
+        int err = 0;
+        if ((err = f_write(&imuFile, log_entry, log_len, &written)) != FR_OK || written != log_len)
+        {
+          printf("Write failed: %s\n", FF_ERRORS[err]);
+          return err;
+        }
+      }
 
-    //   /* Ensure DRDY line cleared before re-enabling */
-    //   while (MXC_GPIO_InGet(IMU_INT_PORT, IMU_INT_MASK))
-    //     ;
-    //   MXC_GPIO_ClearFlags(IMU_INT_PORT, IMU_INT_MASK);
-    //   MXC_GPIO_EnableInt(IMU_INT_PORT, IMU_INT_MASK);
-    // }
+      /* Ensure DRDY line cleared before re-enabling */
+      while (MXC_GPIO_InGet(IMU_INT_PORT, IMU_INT_MASK))
+        ;
+      MXC_GPIO_ClearFlags(IMU_INT_PORT, IMU_INT_MASK);
+      MXC_GPIO_EnableInt(IMU_INT_PORT, IMU_INT_MASK);
+    }
 
     // wsfOsDispatcher();
 
